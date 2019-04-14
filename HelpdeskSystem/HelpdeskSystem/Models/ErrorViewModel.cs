fb097acc0821ba6fbc21e2e434529d3a6af77366
Wade James Russell <wados.russell70@gmail.com>
2019-04-14 12:27:05 +1000

    Initial Empty Project

---
+++ HelpdeskSystem/HelpdeskSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelpdeskSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
