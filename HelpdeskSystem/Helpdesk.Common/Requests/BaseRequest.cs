7bc9e9dfa5a3389f2e34c2b504c70049390033b9
Wade James Russell <wados.russell70@gmail.com>
2019-06-30 17:01:46 +1000

    Set up base project including web app with angular

---
+++ HelpdeskSystem/Helpdesk.Common/Requests/BaseRequest.cs
@@ -0,0 +1,36 @@
+﻿using Helpdesk.Common.Responses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text;
+
+namespace Helpdesk.Common.Requests
+{
+    public class BaseRequest : IValidatableObject
+    {
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            return results;
+        }
+
+        public BaseResponse CheckValidation(BaseResponse response)
+        {
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            ValidationContext validationContext = new ValidationContext(this);
+            bool isValid = Validator.TryValidateObject(this, validationContext, validationResults, true);
+
+            if (!isValid)
+            {
+                response.Status = HttpStatusCode.BadRequest;
+
+                foreach (ValidationResult result in validationResults)
+                    response.StatusMessages.Add(new StatusMessage(HttpStatusCode.BadRequest, result.ErrorMessage));
+            }
+
+            return response;
+        }
+    }
+}
---
+++ HelpdeskSystem/Helpdesk.Common/Responses/BaseResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Helpdesk.Common.Responses
+{
+    /// <summary>
+    /// Used as a base for all responses so that they contain status codes and messages
+    /// </summary>
+    public class BaseResponse
+    {
+        public BaseResponse()
+        {
+            StatusMessages = new List<StatusMessage>();
+        }
+
+        public List<StatusMessage> StatusMessages { get; set; }
+
+        public HttpStatusCode Status = HttpStatusCode.InternalServerError;
+    }
+
+    /// <summary>
+    /// Status message that is used to indicate the results of processes
+    /// </summary>
+    public class StatusMessage
+    {
+        public HttpStatusCode MessageStatus { get; set; }
+
+        public string Message { get; set; }
+
+        public StatusMessage(HttpStatusCode statusCode, string message)
+        {
+            MessageStatus = statusCode;
+            Message = message;
+        }
+    }
+}
