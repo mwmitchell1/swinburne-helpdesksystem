1707bfadaf11ad4ca78086d11b04a29bbda7d66b
Wade James Russell <wados.russell70@gmail.com>
2019-06-30 17:01:46 +1000

    Set up base project including web app with angular

---
+++ HelpdeskSystem/Helpdesk.Website/Controllers/api/BaseApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Helpdesk.Common.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace Helpdesk.Website.Controllers.api
+{
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        protected static Logger s_logger = LogManager.GetCurrentClassLogger();
+
+        protected string buildBadRequestMessage(BaseResponse response)
+        {
+            string message = string.Empty;
+            try
+            {
+                foreach (StatusMessage statusMessage in response.StatusMessages)
+                    message += statusMessage.Message + "\n";
+            }
+            catch (Exception ex)
+            {
+                s_logger.Error(ex, "Unable to build bad request message.");
+                message = string.Empty;
+            }
+
+            return message;
+        }
+    }
+}
