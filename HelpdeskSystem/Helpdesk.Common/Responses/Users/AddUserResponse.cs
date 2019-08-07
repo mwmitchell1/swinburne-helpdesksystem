3ff37fbeca295e9ea46eb3c70211d8221422add3
Andrew Hill <100641690@student.swin.edu.au>
2019-08-07 10:27:42 +1000

    Added Username and Password.

--- HelpdeskSystem/Helpdesk.Common/Responses/Users/AddUserResponse.cs
+++ HelpdeskSystem/Helpdesk.Common/Responses/Users/AddUserResponse.cs
@@ -7,5 +7,7 @@ namespace Helpdesk.Common.Responses.Users
     public class AddUserResponse : BaseResponse
     {
         public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
