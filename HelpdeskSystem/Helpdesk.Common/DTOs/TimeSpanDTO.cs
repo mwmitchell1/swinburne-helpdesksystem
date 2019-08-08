865c0d12dc39654d12149026104e7527aad1f621
Dylan Rossi <101624317@student.swin.edu.au>
2019-08-08 16:52:51 +1000

    Added TimeSpan DTO

--- HelpdeskSystem/Helpdesk.Common/DTOs/TimeSpanDTO.cs
+++ HelpdeskSystem/Helpdesk.Common/DTOs/TimeSpanDTO.cs
@@ -6,5 +6,10 @@ namespace Helpdesk.Common.DTOs
 {
     public class TimeSpanDTO
     {
+        public int SpanId { get; set; }
+        public int? HelpdeskId { get; set; }
+        public string Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
 }
