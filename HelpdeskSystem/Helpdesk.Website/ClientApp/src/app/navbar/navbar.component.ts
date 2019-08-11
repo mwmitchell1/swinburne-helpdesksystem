c03e39521084f4065315aa9cb2cd26db42845550
Pramodh Ganeshan <pormahd@gmail.com>
2019-08-11 19:43:56 +1000

    Added navbar routes

--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/navbar/navbar.component.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/navbar/navbar.component.ts
@@ -4,12 +4,29 @@ import { AuthenticationService } from '../authentication/authentication.service'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
-  //styleUrls: ['./navbar.component.css']
+  // styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   isExpanded = false;
   public authenticationService: AuthenticationService;
   public userIsAuthorized: boolean;
+
+  public selectedHelpdesk: object;
+
+  private helpdesks = [
+    {
+      id: '01',
+      name: 'helpdesk-1'
+    },
+    {
+      id: '02',
+      name: 'helpdesk-2'
+    },
+    {
+      id: '03',
+      name: 'helpdesk-3'
+    }
+  ];
 
   constructor(private service: AuthenticationService) {
     this.authenticationService = service;
@@ -23,4 +40,26 @@ export class NavbarComponent {
   toggle() {
     this.isExpanded = !this.isExpanded;
   }
+
+  /**
+   * This function sets the currently selected helpdesk
+   * @param selected The name of the selected helpdesk
+   */
+  setSelectedHelpdesk(selected: string) {
+    this.helpdesks.forEach((helpdesk) => {
+      if (helpdesk.name.toLowerCase() === selected.toLowerCase()) {
+        this.selectedHelpdesk = helpdesk;
+        return;
+      }
+    });
+  }
+
+  /**
+   * This function checks if a helpdesk is currently selected
+   * @return boolean
+   */
+  helpdeskIsSelected(): boolean {
+    return !(typeof this.selectedHelpdesk === 'undefined');
+  }
+
 }
