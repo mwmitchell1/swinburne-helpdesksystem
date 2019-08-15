6a707deeb891a30275574deee9b48dd72efa61e1
Wade James Russell <wados.russell70@gmail.com>
2019-08-15 10:39:21 +1000

    Started Queue Work

---
+++ HelpdeskSystem/Helpdesk.DataLayer/QueueDataLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpdesk.DataLayer
+{
+    public class QueueDataLayer
+    {
+        public int? AddToQueue()
+        {
+
+        }
+    }
+}
