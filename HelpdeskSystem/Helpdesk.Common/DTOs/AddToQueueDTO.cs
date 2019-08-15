513b7ec683706dd46fa66e9e9f35e5960fe7ef59
Wade James Russell <wados.russell70@gmail.com>
2019-08-15 10:39:21 +1000

    Started Queue Work

---
+++ HelpdeskSystem/Helpdesk.Common/DTOs/AddToQueueDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpdesk.Common.DTOs
+{
+    public class AddToQueueDTO
+    {
+        public int StudentID { get; set; }
+
+        public int TopicID { get; set; }
+    }
+}
