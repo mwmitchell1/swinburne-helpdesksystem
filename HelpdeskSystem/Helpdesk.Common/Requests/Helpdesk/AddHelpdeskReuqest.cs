39a739a9c7e082505f196280968d25726c848b93
Wade James Russell <wados.russell70@gmail.com>
2019-08-16 12:12:57 +1000

    Begun create helpdesk code

---
+++ HelpdeskSystem/Helpdesk.Common/Requests/Helpdesk/AddHelpdeskReuqest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Helpdesk.Common.Requests.Helpdesk
+{
+    public class AddHelpdeskReuqest : BaseRequest
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "You must enter in a helpdesk name.")]
+        public string Name { get; set; }
+
+        public bool HasCheckIn { get; set; }
+
+        public bool HasQueue { get; set; }
+    }
+}
