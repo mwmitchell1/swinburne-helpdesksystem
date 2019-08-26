419d23cbfaff1afe3697d0ad4ba4affb75278420
Pramodh Ganeshan <pormahd@gmail.com>
2019-08-26 22:07:01 +1000

    Added logout button

--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/authentication/logout/logout.component.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/authentication/logout/logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterStateSnapshot, Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
+import { HelpdeskDataService } from '../../helpdesk-data/helpdesk-data.service';
 
 @Component({
   selector: 'app-logout',
@@ -12,13 +13,16 @@ import { AuthenticationService } from '../authentication.service';
  */
 export class LogoutComponent implements OnInit {
 
-  constructor(private userService: AuthenticationService, private router: Router) { }
+  constructor(private userService: AuthenticationService,
+              private router: Router,
+              private helpdeskData: HelpdeskDataService) { }
 
   /**
-   * This function calls the log out function in the auth service 
+   * This function calls the log out function in the auth service
    */
   ngOnInit() {
     this.userService.logout();
-    this.router.navigateByUrl('/');
+    // Navigate to helpdesk view of active helpdesk
+    this.router.navigateByUrl('/helpdesk/' + this.helpdeskData.getActiveHelpdesk().id);
   }
 }
