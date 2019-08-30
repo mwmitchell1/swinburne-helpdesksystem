98617f5125ed16cbf381cf22e29ff366594093bb
Andrew Hill <100641690@student.swin.edu.au>
2019-08-30 21:37:20 +1000

    Added topic functionality.

---
+++ HelpdeskSystem/Helpdesk.Common/Responses/Topics/GetTopicsByUnitIDResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Helpdesk.Common.DTOs;
+
+namespace Helpdesk.Common.Responses.Topics
+{
+    public class GetTopicsByUnitIDResponse : BaseResponse
+    {
+        public List<TopicDTO> Topics { get; set; }
+    }
+}
