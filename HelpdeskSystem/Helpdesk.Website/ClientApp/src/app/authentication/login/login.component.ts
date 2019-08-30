2130523f3dee66ecb10d75dd9ba427376553ad9e
Wade James Russell <wados.russell70@gmail.com>
2019-08-30 15:09:07 +1000

    added UI response feedback to log in form

--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/authentication/login/login.component.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/authentication/login/login.component.ts
@@ -3,6 +3,7 @@ import { LoginRequest } from 'src/app/data/requests/login-request';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-login',
@@ -16,11 +17,16 @@ import { AuthenticationService } from '../authentication.service';
 export class LoginComponent implements OnInit {
   loginForm;
   private returnUrl;
+  private readonly notifier: NotifierService;
 
   constructor(private builder: FormBuilder,
               private service: AuthenticationService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              notifierSerive: NotifierService) {
+
+    this.notifier = notifierSerive;
+    
     this.loginForm = this.builder.group({
       username: '',
       password: ''
@@ -39,13 +45,40 @@ export class LoginComponent implements OnInit {
    * @param data The login inforamtion of the user
    */
   logIn(data) {
+
+    var isValid: boolean = true;
+
+    if (data.username == "")
+    {
+      this.notifier.notify('warning', 'You must enter your username');
+      isValid = false;
+    }
+
+    if (data.password == "")
+    {
+      this.notifier.notify('warning', 'You must enter your password');
+      isValid = false;
+    }
+
+    if (!isValid)
+      return;
+
     const loginRequest = new LoginRequest;
     loginRequest.Password = data.password;
     loginRequest.Username = data.username;
 
     this.service.loginUser(loginRequest).subscribe(result => {
-      console.log(result);
       this.router.navigateByUrl(this.returnUrl);
+    },
+    error => {
+      if (error.status == 400)
+      {
+        this.notifier.notify('warning', 'Username or password is incorrect.');
+      }
+      else
+      {
+        this.notifier.notify('error', 'Unable to login please contact admin.')
+      }
     });
   }
 }
