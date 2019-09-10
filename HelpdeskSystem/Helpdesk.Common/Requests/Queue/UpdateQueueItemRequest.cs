852deb82fb1a1ea0a2ce11b12e4957767423b666
Andrew Hill <100641690@student.swin.edu.au>
2019-09-10 13:18:50 +1000

    Finished UpdateQueueItem and UpdateQueueItemStatus. Added unit tests.

---
+++ HelpdeskSystem/Helpdesk.Common/Requests/Queue/UpdateQueueItemRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helpdesk.Common.Requests.Queue
+{
+    public class UpdateQueueItemRequest : BaseRequest
+    {
+        public int TopicID { get; set; }
+    }
+}
