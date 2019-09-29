6c108758ae76126e558a0a5df6a4f30820d0009e
Pramodh Ganeshan <pormahd@gmail.com>
2019-09-29 22:25:53 +1000

    Refactored units page to use UnitsService

--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/admin/configuration/units/units.service.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/admin/configuration/units/units.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Unit } from '../../../data/DTOs/unit.dto';
+import {GetUnitsByHelpdeskIdResponse} from '../../../data/responses/units/get-by-help-id.response';
 
 @Injectable()
 export class UnitsService {
@@ -9,9 +10,10 @@ export class UnitsService {
 
   /**
    * HTTP request to get active helpdesk's units
+   * @param id The id of the helpdesk to get units for
    */
-  // getUnits() {
-  //   return this.http.get<Unit>('/api/units/helpdesk/1');
-  // }
+  getUnitsByHelpdeskId(id: number) {
+    return this.http.get<GetUnitsByHelpdeskIdResponse>('/api/units/helpdesk/' + id);
+  }
 
 }
