58560ce1630ba7439e69d06f0de0d5e3278bac72
Wade James Russell <wados.russell70@gmail.com>
2019-10-02 11:09:05 +1000

    Added nickname validation api for helpdesk check in and queue joining

---
+++ HelpdeskSystem/Helpdesk.Common/Responses/Students/ValidateNicknameResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpdesk.Common.Responses.Students
+{
+    public class ValidateNicknameResponse : BaseResponse
+    {
+        public int? StudentID { get; set; }
+    }
+}
