c44a8c6c281310ad5cd75381acd49ecdc4c484cd
Wade James Russell <wados.russell70@gmail.com>
2019-10-06 14:40:46 +1100

    Added join queue UI

--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/data/DTOs/check-in.dto.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/data/DTOs/check-in.dto.ts
@@ -1,4 +1,6 @@
 export class CheckIn {
     public checkInId: number;
     public nickname: string;
+    public unitId: number;
+    public studentId: number;
 }
--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/data/responses/helpdesk/check-in.response.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/data/responses/helpdesk/check-in.response.ts
@@ -2,4 +2,5 @@ import { BaseResponse } from "../base-response";
 
 export class CheckInResponse extends BaseResponse {
     public checkInID: number;
+    public studentID: number;
 }
