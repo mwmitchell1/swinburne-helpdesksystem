1af8636a22dae7a73c4d138d1149053593d08300
Pramodh Ganeshan <pormahd@gmail.com>
2019-10-08 19:13:04 +1100

    Added validation for 'New Helpdesk' UI

--- HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/admin/admin.component.ts
+++ HelpdeskSystem/Helpdesk.Website/ClientApp/src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { Helpdesk } from '../data/DTOs/helpdesk.dto';
 import { HelpdeskService } from '../helpdesk/helpdesk.service';
 import { NotifierService } from 'angular-notifier';
@@ -30,13 +30,35 @@ export class AdminComponent {
 
   createHelpdesk(form) {
 
+    let allowSubmit = true;
+
+    // Check if name has been touched - do not allow submit - required to show invalid msg on submit
+    if (!this.createRequest.hasOwnProperty('name')) {
+      form.controls['helpdesk-add-name'].markAsDirty();
+      allowSubmit = false;
+    }
+
+    // Checkbox validation
+    if (!this.createRequest.isDisabled && !this.createRequest.hasCheckIn && !this.createRequest.hasQueue) {
+        allowSubmit = false;
+    }
+
+    // If not allowed to submit, end function
+    if (!allowSubmit) { return; }
+
+    // Allowed to submit - send request
     this.setupService.createHelpdesk(this.createRequest).subscribe(
       result => {
-        console.log(result);
+        $('#modal-helpdesk-add').modal('hide');
+        form.reset();
+
+        this.notifier.notify('success', 'Helpdesk added successfully!');
       }, error => {
         console.log(error);
+        this.notifier.notify('error', 'Could not add new helpdesk, please contact helpdesk admin.');
       }
     );
+
 
   }
 }
